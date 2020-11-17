8b153700dbb866fb861dec97a028f01a5aace1e9
Patrick Gremo <patrick.gremo@gmail.com>
2020-11-17 07:56:57 -0600

    initial view implementation

--- src/test/scala/zozzamas/RegistryTest.scala
+++ src/test/scala/zozzamas/RegistryTest.scala
@@ -20,17 +20,6 @@ type ComponentOf = [V] =>> V match {
     case Storage[a] => a
   }
 
-def extract[T <: Tuple, I <: Tuple.InverseMap[T, Storage]](t: T, i: Entity)(using Tuple.IsMappedBy[Storage][T]): I =
-  t.map {
-    [M] => (m: M) => m.asInstanceOf[Storage[?]](i).asInstanceOf[ComponentOf[M]]
-  }.asInstanceOf[I]
-
-def transform[T <: Tuple, I <: Tuple.InverseMap[T, Storage], B](
-    t: T,
-    f: I => B,
-    i: Entity                                                           
-  )(using Tuple.IsMappedBy[Storage][T]): B  = f(extract(t, i))
-
 def register[C](entity: Entity, value: C)(using storage: Storage[C]): Unit = storage(entity) = value
 
 def get[C](entity: Entity)(using storage: Storage[C]): C = storage(entity)
@@ -42,28 +31,42 @@ inline def view[T <: Tuple]: Tuple = {
   }
 }
 
+type Homogenous[H, T <: Tuple] = T match {
+  case EmptyTuple => DummyImplicit
+  case H *: t => Homogenous[H, t]
+  case ? => Nothing
+}
+
+class View[T <: Tuple, I <: Tuple.InverseMap[T, Storage]](private val source: T)(using Homogenous[Storage[?], T]){
+  def entities: Set[Entity] =
+    source.productIterator
+      .map(_.asInstanceOf[Map[Entity, ?]].keySet)
+      .reduce(_ intersect _)
+
+  def extract(i: Entity)(using Tuple.IsMappedBy[Storage][T]): I =
+    source.map {
+      [M] => (m: M) => m.asInstanceOf[Storage[?]](i).asInstanceOf[ComponentOf[M]]
+    }.asInstanceOf[I]
+
+  def components(using Tuple.IsMappedBy[Storage][T]): Set[I] =
+    entities.map(extract)
+}
+
 class RegistryTest {
   @Test def query(): Unit = {
-    val entity1 = Entity()
+    val entity = Entity()
 
-    register(entity1, 44)
-    register(entity1, "hello")
-    register(entity1, Date())
+    register(entity, 44)
+    register(entity, "hello")
+    register(entity, Date())
     
     val lookup = view[(Int, String, Date)]
     println(lookup)
 
     val items = (intItems, stringItems, dateItems)
 
-    var keys = items.productIterator
-      .map(_.asInstanceOf[Map[Entity, ?]].keySet)
-      .reduce(_ intersect _)
-    println(keys)
-
-    var entities = keys.map(transform(items, x => x, _))
-    println(entities)
-
     var exec = (n: Int, s: String, d: Date) => println(s"${n}, ${s}, ${d}")
-    entities.foreach(exec.tupled)
+    
+    View(items).components.foreach(exec.tupled)
   }
 }
