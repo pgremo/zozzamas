96fa85dc2387ff4f846b3d0ab57700beca90974f
Patrick Gremo <patrick.gremo@gmail.com>
2020-11-17 17:20:34 -0600

    added a type safe get component on view

--- src/test/scala/zozzamas/RegistryTest.scala
+++ src/test/scala/zozzamas/RegistryTest.scala
@@ -15,6 +15,7 @@ type Storage[X] = Map[Entity, X]
 given intItems as Storage[Int] = SparseMap[Int]()
 given stringItems as Storage[String] = SparseMap[String]()
 given dateItems as Storage[Date] = SparseMap[Date]()
+given longItems as Storage[Long] = SparseMap[Long]()
 
 type ComponentOf = [V] =>> V match {
     case Storage[a] => a
@@ -24,20 +25,20 @@ def register[C](entity: Entity, value: C)(using storage: Storage[C]): Unit = sto
 
 def get[C](entity: Entity)(using storage: Storage[C]): C = storage(entity)
 
-inline def view[T <: Tuple]: Tuple = {
+inline def makeView[T <: Tuple]: Tuple = {
   inline erasedValue[T] match {
     case _: EmptyTuple => EmptyTuple
-    case _: (head *: tail) => summonInline[Storage[head]] *: view[tail]
+    case _: (head *: tail) => summonInline[Storage[head]] *: makeView[tail]
   }
 }
 
-type Homogenous[H, T <: Tuple] = T match {
+type Homogenous[T <: Tuple, H] = T match {
   case EmptyTuple => DummyImplicit
-  case H *: t => Homogenous[H, t]
+  case H *: t => Homogenous[t, H]
   case ? => Nothing
 }
 
-class View[T <: Tuple, I <: Tuple.InverseMap[T, Storage]](private val source: T)(using Homogenous[Storage[?], T]){
+class View[T <: Tuple, I <: Tuple.InverseMap[T, Storage]](private val source: T)(using Homogenous[T, Storage[?]]){
   def entities: Set[Entity] =
     source.productIterator
       .map(_.asInstanceOf[Map[Entity, ?]].keySet)
@@ -47,9 +48,12 @@ class View[T <: Tuple, I <: Tuple.InverseMap[T, Storage]](private val source: T)
     source.map {
       [M] => (m: M) => m.asInstanceOf[Storage[?]](i).asInstanceOf[ComponentOf[M]]
     }.asInstanceOf[I]
-
+    
   def components(using Tuple.IsMappedBy[Storage][T]): Set[I] =
     entities.map(extract)
+
+  def get[K <: Tuple.Union[Tuple.InverseMap[T, Storage]]](i: Entity)(using storage: Storage[K]): K =
+    storage(i)
 }
 
 class RegistryTest {
@@ -60,13 +64,15 @@ class RegistryTest {
     register(entity, "hello")
     register(entity, Date())
     
-    val lookup = view[(Int, String, Date)]
+    val lookup = makeView[(Int, String, Date)]
     println(lookup)
 
     val items = (intItems, stringItems, dateItems)
 
     var exec = (n: Int, s: String, d: Date) => println(s"${n}, ${s}, ${d}")
-    
-    View(items).components.foreach(exec.tupled)
+
+    val view = View(items)
+    view.components.foreach(exec.tupled)
+    println(view.get[Date](entity))
   }
 }
