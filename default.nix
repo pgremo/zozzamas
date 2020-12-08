1dd8edf2652c90b1b177868d71a5cb0c51cc20ea
Patrick Gremo <patrick.gremo@gmail.com>
2020-12-08 17:28:04 -0600

    pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,12 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+    nixpkgs = fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/3a02dc9edb283beb9580c9329f242ad705a721c3.tar.gz";
+    };
 
-pkgs.mkShell {
+    pkgs = import nixpkgs {};
+
+    jdk = pkgs.jdk14;
+
+in pkgs.mkShell {
+
   buildInputs = with pkgs; [
     git
-    jdk14
+    jdk
     powershell
     (sbt.override {
-      jre = jdk14;
+      jre = jdk;
     })
   ];
+
 }
