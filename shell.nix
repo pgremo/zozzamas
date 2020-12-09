e47e637eace055cf106ff6cf989ef26d62463c8d
Patrick Gremo <patrick.gremo@gmail.com>
2020-12-09 13:36:24 -0600

    alternate nix configurations

--- shell.nix
+++ shell.nix
@@ -1,9 +1,5 @@
-let
-    nixpkgs = fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/3a02dc9edb283beb9580c9329f242ad705a721c3.tar.gz";
-    };
-
-    pkgs = import nixpkgs {};
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/3a02dc9edb283beb9580c9329f242ad705a721c3.tar.gz") {}
+}:let
 
     jdk = pkgs.jdk14;
     sbt = (pkgs.sbt.override { jre = jdk; });
