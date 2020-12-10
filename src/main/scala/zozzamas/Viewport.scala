8970c7ec26aec9b0809dd2d93f243d6d522fb53f
Patrick Gremo <patrick.gremo@gmail.com>
2020-12-10 08:59:24 -0600

    remove terminal size bounds checking since terminal size can never be less than (0,0)

--- src/main/scala/zozzamas/Viewport.scala
+++ src/main/scala/zozzamas/Viewport.scala
@@ -4,22 +4,16 @@ import com.googlecode.lanterna.gui2.{AbstractInteractableComponent, Interactable
 import com.googlecode.lanterna.{TerminalPosition, TerminalSize, TextCharacter}
 
 class Viewport extends AbstractInteractableComponent[Viewport] {
-  override def createDefaultRenderer: InteractableRenderer[Viewport] = ViewportRenderer()
-
-  class ViewportRenderer extends InteractableRenderer[Viewport] {
+  override def createDefaultRenderer: InteractableRenderer[Viewport] = new InteractableRenderer[Viewport] {
     override def getCursorLocation(component: Viewport | UncheckedNull): TerminalPosition | Null = null
 
     override def getPreferredSize(component: Viewport | UncheckedNull): TerminalSize = TerminalSize(3, 3)
 
     override def drawComponent(graphics: TextGUIGraphics | UncheckedNull, component: Viewport | UncheckedNull): Unit = {
       graphics.getSize() match {
-        case TerminalSize(columns, rows) => if (rows > 0 && columns > 0) graphics.fillRectangle(TerminalPosition.TOP_LEFT_CORNER, graphics.getSize(), TextCharacter('X'))
+        case _: TerminalSize => graphics.fillRectangle(TerminalPosition.TOP_LEFT_CORNER, graphics.getSize(), TextCharacter('X'))
         case _ => throw NullPointerException()
       }
     }
   }
-}
-
-object TerminalSize {
-  def unapply(t: TerminalSize) = (t.getColumns(), t.getRows())
 }
